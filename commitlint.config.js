f713689df48ee66724ba7a72c02ddd23c27bdf52
Mohamed Ameer Noufil N <ameer03062003@gmail.com>
2026-05-21 00:57:37 +0530

    chore: add pre-commit hooks for commit message style

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,7 @@
-export default { extends: ["@commitlint/config-conventional"] };
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "subject-case": [2, "always", "lower-case"],
+    "subject-full-stop": [2, "never", "."],
+  },
+};
